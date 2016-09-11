c0076ef15db45074edda4e119117b99b2cded210
Bytegineer <Bytegineer@users.noreply.github.com>
2016-09-11 16:39:17 -0500

    Broke client functions into subthread

--- labs/lab01/lab01/src/Client.java
+++ labs/lab01/lab01/src/Client.java
@@ -17,77 +17,89 @@ public class Client {
 	public static void main(String[] args) {
 		String name;
 
-//		Scanner scanny = new Scanner(System.in);
 		System.out.println("What is your username? ");
 		name = scan.nextLine();
 		Client client = new Client("localhost", name, 1222);
 
 	}
 	
-
-
-public Client(String ipAddr, String username, int serverPort)
-{
-
-		// set up the socket to connect to the gui
-		try {
-			socket = new Socket(ipAddr, serverPort);
-			out = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
-			start();
-		} catch (UnknownHostException h) {
-			System.exit(1);
-		} catch (IOException e) {
-			System.exit(1);
-
-		}
-}
 	
+	private static class ClientThread implements Runnable
+	{
+		private Socket socket = null;
+		private DataOutputStream streamOut = null;
 		
-public void run() {
-		// TODO check for a new message, once we receive it, steamOut will send
-		// it to the server
-		String answer;
-		String message;
-			
-
-		while (true) {
-
-			System.out.println("Press '1' to send message, '2' for image.");
-			answer = scan.nextLine();
-			
-			if (answer.equals("1")) {
-				System.out.print("Enter your message: ");
-				message = scan.nextLine();
-				handleChat(message);
-			} else if (answer.equals("2")) {
-
-			} else {
-				System.out.println("Invalid input, try again.");
+	
+		public ClientThread(String ipAddr, String username, int serverPort)
+		{
+	
+			// set up the socket to connect to the gui
+			try {
+				socket = new Socket(ipAddr, serverPort);
+				out = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
+				start();
+			} catch (UnknownHostException h) {
+				System.out.println("Unknown Host " + h.getMessage());
+				System.exit(1);
+			} catch (IOException e) {
+				System.out.println("IO exception: " + e.getMessage());
+				System.exit(1);
+	
 			}
-			answer = null;
 		}
-
-	}
-
-	public synchronized void handleChat(String message) {
-		out.println(username + " says " + message);
-		out.flush();
-//		out.close();
-	}
-
-	public void start() throws IOException {
-
-		out.println(username + ":endUsername");
-		out.flush(); // forces data from buffer to be sent to server
-//		out.close();
-
-		run();
-
-	}
-
-	public void stop() {
-		// TODO
-
-
+		
+			
+		public void run() {
+			// TODO check for a new message, once we receive it, steamOut will send
+			// it to the server
+			String answer;
+			String message;
+				
+	
+			while (true) {
+	
+				System.out.println("Press '1' to send message, '2' for image.");
+				answer = scan.nextLine();
+				
+				if (answer.equals("1")) {
+					System.out.print("Enter your message: ");
+					
+					if (scan.hasNext())
+					{
+						message = scan.nextLine();
+						handleChat(message);
+					}
+					
+				} else if (answer.equals("2")) {
+					//TODO implement image encoding
+				} else {
+					System.out.println("Invalid input, try again.");
+				}
+				answer = null;
+			}
+	
+		}
+	
+		public synchronized void handleChat(String message) {
+			out.println(username + " says " + message);
+			out.flush();
+	//		out.close();
+		}
+	
+		public void start() throws IOException {
+	
+			out.println(username + ":endUsername");
+			out.flush(); // forces data from buffer to be sent to server
+	//		out.close();
+	
+			run();
+	
+		}
+	
+		public void stop() {
+			// TODO
+	
+	
+		}
 	}
 }
