58aec05e9c21817fbf1f7c287a708cbd7d5cbea0
Bytegineer <Bytegineer@users.noreply.github.com>
2016-09-18 14:12:24 -0500

    Finished validation on form 1, saved info to cookies, started work on form 2.

--- labs/lab02/validation1.js
+++ labs/lab02/validation1.js
@@ -1,4 +1,5 @@
 var submitBtn = document.getElementById("submit");
+var nextBtn = document.getElementById("nextPage");
   submitBtn.onclick = function () {
      // show all elements on clicking submit!
      var myForm = document.getElementById("form1");
@@ -17,38 +18,57 @@ var submitBtn = document.getElementById("submit");
 			   document.getElementById("firstWrong").hidden = false;
 			   document.getElementById("firstCorrect").hidden = true;
 			   successfulValidation = false;
+			   document.cookie = ("firstName=; expires=Fri, 16 Sept 2016 00:00:00 UTC");
 		   } else {
 			   document.getElementById("firstWrong").hidden = true;
 			   document.getElementById("firstCorrect").hidden = false;
+			   document.cookie = ("firstName=" + myForm.elements[i].value);
 		   }
 	   } else if(myForm.elements[i].name == 'lastName'){
 		   if (reg.test(myForm.elements[i].value)){
 			   document.getElementById("lastWrong").hidden = false;
 			   document.getElementById("lastCorrect").hidden = true;
 			   successfulValidation = false;
+			   document.cookie = ("lastName=; expires=Fri, 16 Sept 2016 00:00:00 UTC");
 		   } else {
 			   document.getElementById("lastWrong").hidden = true;
 			   document.getElementById("lastCorrect").hidden = false;
+			   document.cookie = ("lastName=" + myForm.elements[i].value);
 		   }
 	   } else if(myForm.elements[i].name == 'gender'){
 		   if (myForm.elements[i].value == ''){
 			   document.getElementById("genderWrong").hidden = false;
 			   document.getElementById("genderCorrect").hidden = true;
 			   successfulValidation = false;
+			   document.cookie = ("gender=; expires=Fri, 16 Sept 2016 00:00:00 UTC");
 		   } else {
 			   document.getElementById("genderWrong").hidden = true;
 			   document.getElementById("genderCorrect").hidden = false;
+			   document.cookie = ("gender=" + myForm.elements[i].value);
 		   }
 	   } else if(myForm.elements[i].name == 'state'){
 		   if (myForm.elements[i].value == ''){
 			   document.getElementById("stateWrong").hidden = false;
 			   document.getElementById("stateCorrect").hidden = true;
 			   successfulValidation = false;
+			   document.cookie = ("state=; expires=Fri, 16 Sept 2016 00:00:00 UTC");
 		   } else {
 			   document.getElementById("stateWrong").hidden = true;
 			   document.getElementById("stateCorrect").hidden = false;
+			   document.cookie = ("state=" + myForm.elements[i].value);
 		   }
 	   }
      }
-     successfulValidation ? alert (s + "has been submitted!") : alert ("There are errors in the values you entered.\nPlease enter only letters and numbers, complete all fields, and try again.");
+     if (successfulValidation) {	   
+	   nextBtn.hidden = false;
+     	   alert (s + "has been submitted!");
+     } else {
+	   nextBtn.hidden = true;
+	   alert ("There are errors in the values you entered.\nPlease enter only letters and numbers, complete all fields, and try again.");
+     }
   }
+  
+  nextPage.onclick = function () {
+     window.location = "./validation2.html";
+  }
+      
