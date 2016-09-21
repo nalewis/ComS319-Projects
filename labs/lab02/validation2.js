a6bb1667ac9ae7666bd16e6dec5558a22a71b878
Bytegineer <Bytegineer@users.noreply.github.com>
2016-09-21 18:49:35 -0500

    Form2 validation beginnings

---
+++ labs/lab02/validation2.js
@@ -0,0 +1,15 @@
+function validate(){
+	var submitBtn = document.getElementById("submit");
+	var myForm = document.getElementById("form2");		
+
+	var regex = /[^A-Za-z0-9 ]/
+	var i = 0;
+	var formElements = myForm.elements;
+
+	for (i = 0; i < formElements.length; i++){
+		if (formElements[i].name && formElements[i].name == "email"){
+			
+		}
+	
+	}	
+}
