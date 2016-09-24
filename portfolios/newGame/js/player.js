fa8a0be3ddd54660681602c2100b38b29658e0f6
Lewis <nalewis@iastate.edu>
2016-09-24 18:46:22 -0500

    update for evan

---
+++ portfolios/newGame/js/player.js
@@ -0,0 +1,26 @@
+var Player = function (pic, x, y, width, height)
+{
+	//Thing.call(this, document.getElementById("player"), x, y, $("#player").width(), $("#player").height());
+	this.pic = pic;
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;
+	this.deg = -90;
+	this.turnSpeed = 4;
+	this.bulletSpeed = 10;
+	var lastCheck = false;
+	var maxSpeed = 50;
+	this.playerSpeed = 15;
+	this.lastSpeed = 10;
+
+
+	this.draw = function (can)
+	{
+		console.log("player field");
+		//this.drawImageRot(this.pic, this.x, this.y, this.width, this.height, this.deg);
+		//console.log(this.pic);
+		//console.log(this.x);
+		can.drawImage(this.pic, this.x, this.y);
+	};
+}
