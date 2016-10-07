517e6fe9226161092b00997c83948b0d619b9c94
Lewis <nalewis@iastate.edu>
2016-10-07 14:30:38 -0500

    lab4 basic skeleton

---
+++ labs/lab04/code.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('p').click(function(){
+		$('p').hide("slow");
+	});
+	$('#d1').hover(function(){
+		$('#d1').css("color", "red");
+	});
+	$('#d1').hover(function(){
+		$('#d1').css("color", "red");
+	});
+	$('#d1').hover(function(){
+		$('#d1').css("color", "red");
+	});
+	$('#d1').hover(function(){
+		$('#d1').css("color", "red");
+	});
+});
