5f412a45e20f5455186b5eb9ab2b1c8fed892589
Lewis <nalewis@iastate.edu>
2016-10-07 15:00:48 -0500

    part 1

--- labs/lab04/code.js
+++ labs/lab04/code.js
@@ -5,13 +5,27 @@ $(document).ready(function(){
 	$('#d1').hover(function(){
 		$('#d1').css("color", "red");
 	});
-	$('#d1').hover(function(){
-		$('#d1').css("color", "red");
+	$('#imp').keydown(function(){
+		$('#imp').css("background-color", "green");
 	});
-	$('#d1').hover(function(){
-		$('#d1').css("color", "red");
+	$('#imp').keyup(function(){
+		$('#imp').css("background-color", "blue");
 	});
-	$('#d1').hover(function(){
-		$('#d1').css("color", "red");
+	$('.school').hover(function(){
+		$('.school').slideUp(200);
 	});
+	$('#d3').hover(function(){
+		$('#d3').css({"color": "red",
+					"background-color": "#98bf21",
+					"font-family": "Arial",
+					"font-size": "20px",
+					"font-style": "italic"
+					});
+	});
+	$('#butt').dblclick(function (){
+		$('#toggler').toggle();
+	});
+	
+	$('#d4').fadeIn(1000);
+	$('#d5').fadeOut(2000);
 });
