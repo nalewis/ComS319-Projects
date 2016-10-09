e86348723b6104bf591ad3fdba76e5b558b12929
Nick Lewis <nalewis@iastate.edu>
2016-10-08 20:23:26 -0600

    added display functionality

--- labs/lab04/library/booksLibrary.js
+++ labs/lab04/library/booksLibrary.js
@@ -13,50 +13,87 @@ function Shelf(type) {
 
 function Library() {
 	this.shelves;
-}
-
-function initializeLibrary(library, shelves, books) {
-	library.shelves = shelves;
-	for(i = 0; i < books.length; i++){
-		if(books[i].id % 4 == 0){
-			for(j = 0; j < library.shelves.length; j++){
-				if(library.shelves[j].type == 'Art'){
-					library.shelves[j].books.push(books[i]);
+	this.initialize = function(shelves, books){
+		this.shelves = shelves;
+		for(i = 0; i < books.length; i++){
+			if(books[i].id % 4 == 0){
+				for(j = 0; j < this.shelves.length; j++){
+					if(this.shelves[j].type == 'Art'){
+						this.shelves[j].books.push(books[i]);
+					}
 				}
 			}
+			if(books[i].id % 4 == 1){
+				for(j = 0; j < this.shelves.length; j++){
+					if(this.shelves[j].type == 'Science'){
+						this.shelves[j].books.push(books[i]);
+					}
+				}
+			}
+			if(books[i].id % 4 == 2){
+				for(j = 0; j < this.shelves.length; j++){
+					if(this.shelves[j].type == 'Sport'){
+						this.shelves[j].books.push(books[i]);
+					}
+				}
+			}
+			if(books[i].id % 4 == 3){
+				for(j = 0; j < this.shelves.length; j++){
+					if(this.shelves[j].type == 'Literature'){
+						this.shelves[j].books.push(books[i]);
+					}
+				}
+			}		
 		}
-		if(books[i].id % 4 == 1){
-			for(j = 0; j < library.shelves.length; j++){
-				if(library.shelves[j].type == 'Science'){
-					library.shelves[j].books.push(books[i]);
-				}
-			}
-		}
-		if(books[i].id % 4 == 2){
-			for(j = 0; j < library.shelves.length; j++){
-				if(library.shelves[j].type == 'Sport'){
-					library.shelves[j].books.push(books[i]);
-				}
-			}
-		}
-		if(books[i].id % 4 == 3){
-			for(j = 0; j < library.shelves.length; j++){
-				if(library.shelves[j].type == 'Literature'){
-					library.shelves[j].books.push(books[i]);
-				}
-			}
-		}		
 	}
-	return library;
+	
+	this.display = function(){
+		var s = "";
+		var longest = 0;
+		for(i = 0; i < this.shelves.length; i++){
+			if(this.shelves[i].books.length > longest){
+				longest = this.shelves[i].books.length;
+			}
+		}
+
+		s += "<table border=2>";
+		s += "<tr>";
+		s += "<td>Art</td>";
+		s += "<td>Science</td>";
+		s += "<td>Sport</td>";
+		s += "<td>Literature</td>";
+		s += "</tr>";
+		for(j = 0; j < longest; j++){
+			s += "<tr>";
+			for(i = 0; i < this.shelves.length; i++){
+				s += "<td>";
+				if(this.shelves[i].books.length > j){
+					s += this.shelves[i].books[j].id;
+				}
+				s += "</td>";
+			}
+			s += "</tr>"
+		}
+		s += "</table>";
+
+		
+		console.log(s);
+		return s;
+	}
 }
 
 var book1 = new Book(1, 'Reference');
+var book2 = new Book(2, 'Reference');
+var book3 = new Book(3, 'Reference');
+var book4 = new Book(4, 'Reference');
+var book5 = new Book(5, 'Reference');
 var artShelf = new Shelf('Art');
 var sciShelf = new Shelf('Science');
 var sportShelf = new Shelf('Sport');
 var litShelf = new Shelf('Literature');
-var bookArray = [book1];
+var bookArray = [book1, book2, book3, book4, book5];
 var shelfArray = [artShelf, sciShelf, sportShelf, litShelf];
 var library = new Library();
 
-console.log(JSON.stringify(initializeLibrary(library, shelfArray, bookArray)));
+library.initialize(shelfArray, bookArray);
+console.log(JSON.stringify(library));
