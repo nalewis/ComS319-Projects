9ecea6b2458b9d1dd275872b07219304f74b21d9
Nick Lewis <nalewis@iastate.edu>
2016-10-11 15:09:50 -0600

    Attempting to create user object

--- labs/lab04/library/js/login.js
+++ labs/lab04/library/js/login.js
@@ -3,16 +3,18 @@ function verifyInput()
 	if(($("#username").val() == 'admin') && ($("#password").val() == 'admin'))
 	{
 		document.cookie = ("role=admin");
+		
 		$("#failure").hide(500);
-
-		window.location = "./index.html";
+		var user = new User($("#username").val(), getCookie("role"));
+		window.location = "./index.html?User="+user;
 	}
 	else if (($("#username").val().substring(0,1).toLowerCase()) == "u")
 	{
 		document.cookie = ("role=undergraduate");
 		$("#failure").hide(500);
 
-		window.location = "./index.html";
+		var user = new User($("#username").val(), getCookie("role"));
+		window.location = "./index.html?User="+user;
 	}
 	else
 	{
@@ -20,4 +22,20 @@ function verifyInput()
 		$("#failure").show(500);
 	} 
 
+}
+
+//taken from stack overflow http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
 }
