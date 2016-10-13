e9fb75fe1b1bd3756658fa4c3f051f81490285c4
Nick Lewis <nalewis@iastate.edu>
2016-10-13 02:19:26 -0600

    implemented localStorage

--- labs/lab04/library/js/booksLibrary.js
+++ labs/lab04/library/js/booksLibrary.js
@@ -24,7 +24,13 @@ function Library() {
 	this.initialize = function(){
 		//initialize books
 		for(i = 0; i < 20; i++){
-			this.books.push(new Book(i, ''));
+			var book = new Book(i, '');
+			//check local storage to see if this book was checked out and set values accordingly
+			if(localStorage.getItem(i)){
+				book.presence = 0;
+				book.borrowedBy = localStorage.getItem(i);
+			}
+			this.books.push(book);
 		}
 		for(i = 0; i < 5; i++){
 			this.books.push(new Book(i, 'Reference'));
@@ -84,21 +90,28 @@ function Library() {
 		for(j = 0; j < longest; j++){
 			s += "<tr>";
 			for(i = 0; i < this.shelves.length; i++){
-				s += "<td>";
 				if(this.shelves[i].books.length > j){
 					if(this.shelves[i].books[j].type == 'Reference'){
+						s += "<td>";
 						s += "R";
 					} else {
+						//check if localStorage set this book to checked out (not relevant for Reference books)
+						if(this.shelves[i].books[j].presence == 0){
+							s += "<td bgcolor=\"red\">";
+						} else {//else the cell should be white
+							s += "<td>";
+						}
 						s += "B";
 					}
 					s += this.shelves[i].books[j].id;
+				} else {//blank cell
+					s += "<td>";
 				}
 				s += "</td>";
 			}
 			s += "</tr>"
 		}
 
-		//console.log(s);
 		return s;
 	}
 	
@@ -120,13 +133,14 @@ function Library() {
 							if(library.shelves[i].books[k].id == id && type == library.shelves[i].books[k].type){
 								//if librarian, display book info, else do undergrad actions
 								if(library.user.name == 'admin'){
+									//If book is on shelf
 									if(library.shelves[i].books[k].presence){
 										if(type == ""){
 											alert("Book ID: " + library.shelves[i].books[k].id + " Type: Book is present on the " + library.shelves[i].type + " shelf.");
 										} else {
 											alert("Book ID: " + library.shelves[i].books[k].id + " Type: " + type + " is present on the " + library.shelves[i].type + " shelf.");
 										}
-									} else {
+									} else { //If book is checked out
 										if(type == ""){
 											alert("Book ID: " + library.shelves[i].books[k].id + " Type: Book is checked out from the " + library.shelves[i].type + " shelf by " + library.shelves[i].books[k].borrowedBy + ".");
 										} else {
@@ -140,27 +154,32 @@ function Library() {
 										if(library.shelves[i].books[k].presence){
 											//check that the user doesn't already have 2 books checked out
 											if(library.user.borrowedBooks.length < 2){
+												//Mark the action in localStorage
+												localStorage.setItem(library.shelves[i].books[k].id, library.user.name);
+
+												//set values on both book and user
 												library.shelves[i].books[k].borrowedBy = library.user.name;
 												library.user.borrowedBooks.push(library.shelves[i].books[k]);
 												library.shelves[i].books[k].presence = 0;
-												//console.log(library.shelves[i].books[k]);
-												//console.log(library.user);
 												this.style.backgroundColor = "red";
 											} else {
 												alert("You may not check out more than two books at a time.");
 											}
 										} else {//otherwise, check if this is a undergrad returning, or if they need to be notified that the book is gone
 											if(library.shelves[i].books[k].borrowedBy == library.user.name){
+												//mark the action in localStorage
+												localStorage.removeItem(library.shelves[i].books[k].id);
+
+												//set values on both book and user
 												library.shelves[i].books[k].borrowedBy = '';
+												library.shelves[i].books[k].presence = 1;
+												this.style.backgroundColor = "white";
+												//remove book from user borrowedBooks array
 												for(m = 0; m < library.user.borrowedBooks.length; m++){
 													if(library.user.borrowedBooks[m].id == id){
 														library.user.borrowedBooks.splice(m,1);
 													}
 												}
-												library.shelves[i].books[k].presence = 1;
-												//console.log(library.shelves[i].books[k]);
-												//console.log(library.user);
-												this.style.backgroundColor = "white";
 											} else {
 												alert("Book " + library.shelves[i].books[k].id + " has already been checked out by " + library.shelves[i].books[k].borrowedBy);
 											}
@@ -178,33 +197,25 @@ function Library() {
 	
 	//check the login information and display the library if correct
 	this.verifyInput = function(){
-		
-		if(($("#username").val() == 'admin') && ($("#password").val() == 'admin'))
-		{
-			document.cookie = ("role=admin");
+		if(($("#username").val() == 'admin') && ($("#password").val() == 'admin')){
 			$('#libraryDiv').show(100);
 			$('#loginDiv').hide(100);
-			$("#failure").hide(500);
+			$("#failure").hide(100);
 			
 			this.user = new User($("#username").val(), 'admin');
 		}
-		else if (($("#username").val().substring(0,1).toLowerCase()) == "u")
-		{
-			document.cookie = ("role=undergraduate");
+		else if (($("#username").val().substring(0,1).toLowerCase()) == "u"){
 			$('#libraryDiv').show(100);
 			$('#loginDiv').hide(100);
-			$("#failure").hide(500);
+			$("#failure").hide(100);
 	
 			this.user = new User($("#username").val(), 'undergraduate');
-		}
-		else
-		{
-			document.cookie = ("role=; expires=Thu, 01 Jan 2015 00:00:00 UTC");
-			$("#failure").show(500);
+		} else {
+			$("#failure").show(100);
 		} 
 	}
+
 	this.logout = function(){
-		document.cookie = ("role=; expires=Thu, 01 Jan 2015 00:00:00 UTC");
 		$('#libraryDiv').hide(100);
 		$('#loginDiv').show(100);
 		this.user = '';
