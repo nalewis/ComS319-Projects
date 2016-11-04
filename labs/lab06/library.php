e4325d132b7b5df0a6e01558ed7b88143083f01e
Bytegineer <Bytegineer@users.noreply.github.com>
2016-11-03 22:05:06 -0500

    Implemented librarian options on library.php page. Began the jquery to support librarian options.

--- labs/lab06/library.php
+++ labs/lab06/library.php
@@ -23,8 +23,29 @@ function updateDisplay(){
 </head>
 <body>
 <?php var_dump($_SESSION); ?>
+<!-- TODO: Hide the options div if the user isn't a librarian -->
+<div id="options">
+	<h3>Librarian options:</h3>
+	<div id = "addForm">
+		<h4>Add a Book</h4>
+			Book title:<input type = "text" id="title">
+			Book author:<input type = "text" id="author">
+			<button id = "addSubmit">Add Book</button>
+	</div>
+	<div id = "deleteForm">
+		<h4>Delete a Book</h4>
+			Book ID:<input type = "text" id = "bookid">
+			<button id = "deleteSubmit">Delete Book</button>
+	</div>
+	<div id = "historyForm">
+		<h4>View Borrow History</h4>
+			Username:<input type = "text" id = "targetUsername">
+			<button id = "historySubmit">View Borrow History</button>
+	</div>
 
+</div>
 <div id="posts">
+	<h4>Shelves</h4>
 	<?= updateDisplay(); ?>
 </div>
 
@@ -32,3 +53,24 @@ function updateDisplay(){
 
 </body>
 </HTML>
+
+<script>
+	$("#addSubmit").click( function(){
+		$.post("library.php", {type: "addBook", title: $("#title").val(), author: $("#author").val()},
+			function(){
+				//success function here
+			});
+	});
+	$("#deleteSubmit").click( function(){
+		$.post("library.php", {type: "deleteBook", id: $("#bookid").val()},
+			function(){
+				//success function here
+			});
+	});
+	$("#historySubmit").click( function(){
+		$.post("library.php", {type: "history", title: $("#targetUsername").val()},
+			function(){
+				//success function here
+			});
+	});
+</script>
